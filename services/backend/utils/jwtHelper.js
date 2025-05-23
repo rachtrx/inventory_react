0b0207c258247155a68587d916ed544220b51ec3
rachtrx <rachmielteorenxiang@gmail.com>
2025-05-23 18:43:00 +0800

    update duration

--- services/backend/utils/jwtHelper.js
+++ services/backend/utils/jwtHelper.js
@@ -5,7 +5,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { id: user.id, email: user.email }, // Include any necessary claims
     process.env.JWT_SECRET,
-    { expiresIn: '1m' }
+    { expiresIn: '1h' }
   );
 };
 
